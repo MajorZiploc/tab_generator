ceeb023ba46af6ae305393bcfbd91fa67ba5b8c9
Manyu Lakhotia <lakemonyew@gmail.com>
2021-09-11 12:49:59 -0500

    Impls fillInNotes

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,16 @@ import { Tab, Note } from './models';
 import { jsonRefactor as jr } from 'json-test-utility';
 
 function fillInNotes(notes: Note[], numOfStrings: number) {
+  // TODO: Assumes that the notes list doesnt have duplicate stringNum or string Nums outside of the num of strings
+  let currentString = 1;
+  while (numOfStrings >= currentString) {
+    if (!notes.some(n => n.stringNum === currentString)) {
+      // Add the missing string
+      const note: Note = { stringNum: currentString };
+      notes.push(note);
+    }
+    currentString++;
+  }
   return notes;
 }
 
@@ -11,8 +21,10 @@ function main(tab: Tab) {
   const numOfStrings = tab.tuning.split('-').length;
   console.log(numOfStrings);
   // Set missing times to 1
-  fullTab = { tabMap: tab.tabMap.map(t => ({ times: t.times ?? 1, ...t })), ...tab };
-  fullTab = { tabMap: tab.tabMap.map(t => ({ notes: fillInNotes(t.notes, numOfStrings), ...t })), ...tab };
+  fullTab = {
+    tabMap: tab.tabMap.map(t => ({ times: t.times ?? 1, notes: fillInNotes(t.notes, numOfStrings), ...t })),
+    ...tab,
+  };
   console.log(JSON.stringify(fullTab, null, 2));
 }
 
