d706e1496f5f7675fa9b4521baf369849d6a25d6
Manyu Lakhotia <lakemonyew@gmail.com>
2021-09-11 11:10:39 -0500

    Adds numOfStrs and adding times for missing times

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
 import * as tab from './tab1.json';
 import { Tab } from './models';
+import { jsonRefactor as jr } from 'json-test-utility';
 
-function main(tab: Tab) {}
+function main(tab: Tab) {
+  let fullTab = tab;
+  const numOfStrings = tab.tuning.split('-').length;
+  console.log(numOfStrings);
+  // Set missing times to 1
+  fullTab = { tabMap: tab.tabMap.map(t => ({ times: t.times ?? 1, ...t })), ...tab };
+  console.log(JSON.stringify(fullTab, null, 2));
+}
 
 main(tab);
